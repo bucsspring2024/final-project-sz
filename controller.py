faa61e21f51c23186a3b23bb9f5244892cfc11d9
Shoshana <shoshana@Shoshanas-MacBook-Air.local>
2024-04-29 23:55:49 -0400

    new

--- controller.py
+++ controller.py
@@ -1,27 +1,32 @@
 import pygame
-from player import Player
-from computerplayer import ComputerPlayer
-from game import Game
-
+from src.player import Player
+from src.enemy import Enemy
 class Controller:
-    def __init__(self):
-        """ initializes controller
-        """
+    def __init__(self, image_file):
         pygame.init()
+        self.image_file = image_file
+    
     def mainloop(self):
-        """
-        """
-        while True: 
-            #1. Handle events
+        screen = pygame.display.set_mode((1080, 607))
+        background = pygame.image.load(self.image_file)
+        player = Player(screen, 390, 400,"./assets/player.webp")
+        enemy = Enemy(screen, 390, 0, "./assets/enemy/png")
+        while(True):
+            screen.blit(background, (0, 0))
+            player.draw()
+            enemy.draw()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit()
                     
-            #2. detect collisions and update models 
-            
-            #3. Redraw next frame
-            
-            #4. Display next frame 
+            pressed = pygame.key.get_pressed()
+            if(pressed[pygame.K_LEFT]):
+                player.move_left()
+            if(pressed[pygame.K_RIGHT]):
+                player.move_left()
+            if(pressed[pygame.K_SPACE]):
+                player.shoot(enemy)
+            enemy.move(player)
             pygame.display.flip()
             
