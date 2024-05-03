f18e7ecd8526d92fdca3d291f44f35b3df286d55
Shoshana <shoshana@Shoshanas-MacBook-Air.local>
2024-05-02 22:31:28 -0400

    h

--- src/game_over.py
+++ src/game_over.py
@@ -7,6 +7,14 @@ class GameOver:
         self.msg_rect = self.msg_surface.get_rect(center=(screen.get_wodth() // 2, screen.get_height() // 2))
         
     def display(self):
+        """displays game over message
+        
+        Args: 
+            None
+            
+        Returns:
+            None
+        """
         self.screen.fill(0, 0, 0)
         self.screen.blit(self.msg_surface, self.msg_rect)
         pygame.display.flip()
