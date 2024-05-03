5c651aee40008d9eb492d1f2fc5171d122b6f3b7
Shoshana <shoshana@Shoshanas-MacBook-Air.local>
2024-05-02 22:30:28 -0400

    z

--- src/start_menu.py
+++ src/start_menu.py
@@ -2,6 +2,13 @@ import pygame
 
 class StartMenu:
     def __init__(self, screen):
+        """initalizes StartMenu obj
+
+        Args:
+            screen (pygame.Surface): start menu displayed on this surface
+            
+        Returns: None
+        """
         self.screen = screen
         self.font = pygame.font.Font(None, 36)
         self.instr_text = "Press Space to start, or press Q to quit"
@@ -9,6 +16,14 @@ class StartMenu:
         self.instr_rect = self.instr_surface.get_rect(center=(screen.get_wdith() // 2, screen.get_height() // 2))
         
     def display(self):
+        """enables start menu to show up on screen
+        
+        Args:
+            None
+            
+        Returns:
+            None
+        """
         self.screen.fill((0, 0, 0))
         self.screen.blit(self.instr_surface, self.instr_rect)
         pygame.display.flip()
