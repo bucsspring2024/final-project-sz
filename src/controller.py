471e8caa9e12fc3e09108486926e1941f767ac78
Shoshana <shoshana@Shoshanas-MacBook-Air.local>
2024-05-02 22:53:19 -0400

    g

--- src/controller.py
+++ src/controller.py
@@ -30,7 +30,7 @@ class Controller:
         background = pygame.image.load(self.image_file)
         
         player = Player(self.screen, 390, 400,"./assets/player.webp")
-        enemy = Enemy(self.screen, 390, 0, "./assets/enemy/png")
+        enemy = Enemy(self.screen, 390, 0, "./assets/enemy.png")
         
         running = True 
         while running:
